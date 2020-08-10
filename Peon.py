731025443ccdc20355ec271052a243e8259b22d2
Nikita Shymberg <nikiit101@gmail.com>
2020-08-10 09:21:42 -0700

    [WIP] Redid the way that Peon stats work

--- Peon.py
+++ Peon.py
@@ -5,13 +5,19 @@ from utils import Point, shortest_path, distance_between
 from Sprite import Sprite
 from Food import Food
 import numpy as np
+# TODO: something something pdb?
 
 
 class Peon(Sprite):
     def __init__(self, env, my_map, position=Point(0, 0)):
         super().__init__(env, my_map, position)
-        self.speed = 1
-        self.hunger = 100  # TODO: decrement this at some rate TODO: maybe have a separate "process" for this actually
+        self.stats = {
+            'mitosis_progress': 0,
+            'mitosis_efficiency': 0.5,
+            'mitosis_threshold': 300,
+            'speed': 1,
+        }
+        self.hunger = 100  # TODO: decrement this at some rate maybe have a separate "process" for this actually
         self.destination = None
         self.action = self.env.process(self.run())
 
@@ -48,17 +54,27 @@ class Peon(Sprite):
             self.print('Walked')
         else:
             self.print('No destination, stood still')
-        return self.env.timeout(self.speed)  # FIXME: this is backwards, at the moment the higher your speed, the longer you wait to walk
+        return self.env.timeout(self.stats['speed'])  # FIXME: this is backwards, at the moment the higher your speed, the longer you wait to walk
+
+    def mitose(self):
+        """
+        Makes a slightly altered copy of the Peon
+        """
+        ...  # TODO: implement
 
     def eat(self):
         my_food = self.find_food()
         self.hunger += my_food.filling
+        self.stats['mitosis_threshold'] += my_food.filling * self.stats['mitosis_efficiency']
+        if self.stats['mitosis_progress'] >= self.stats['mitosis_threshold']:
+            self.mitose()
         self.destination = None
         self.print('Ate')
         my_food.existance.interrupt()
-        return self.env.timeout(self.speed)  # FIXME: this is backwards, at the moment the higher your speed, the longer you wait to walk
+        return self.env.timeout(self.stats['speed'])  # FIXME: this is backwards, at the moment the higher your speed, the longer you wait to walk
 
     def run(self):
+        # TODO: rename this probably
         while True:
             action = self.determine_action()
             yield action()
